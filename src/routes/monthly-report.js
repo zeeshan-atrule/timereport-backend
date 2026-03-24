e4b76e884ce47fadd7f9fba18f48867be79692e0
zeeshan-atrule <zeeshan.khalid@atrule.com>
2026-03-24 14:22:29 +0500

    commit last day check code

--- src/routes/monthly-report.js
+++ src/routes/monthly-report.js
@@ -2,14 +2,14 @@ import { runMonthlyReportJob } from '../cronMonthlyReport.js';
 
 export default async function handler(req, res) {
   try {
-    const today = new Date()
-    const tomorrow = new Date(today)
-    tomorrow.setDate(today.getDate() + 1)
-    const isLastDay = today.getMonth() !== tomorrow.getMonth()
+    // const today = new Date()
+    // const tomorrow = new Date(today)
+    // tomorrow.setDate(today.getDate() + 1)
+    // const isLastDay = today.getMonth() !== tomorrow.getMonth()
 
-    if (!isLastDay) {
-      return res.status(200).json({ success: false, message: 'Not last day of month, skipping job.' })
-    }
+    // if (!isLastDay) {
+    //   return res.status(200).json({ success: false, message: 'Not last day of month, skipping job.' })
+    // }
 
     await runMonthlyReportJob('vercel-cron')
     res.status(200).json({ success: true })
